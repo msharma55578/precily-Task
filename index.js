ddb6ffe5beb63db5afe012aa5499ec79ae8188e2
msharma55578 <m.sharma55578@gmail.com>
2020-11-04 03:48:56 +0530

    pre final

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let cors = require("cors");
 app.use(cors());
 let bodyParser = require("body-parser");
 app.use(bodyParser());
+
 // Connect create with Mysql
 var con = mysql.createConnection({
   host: "localhost",
@@ -12,6 +13,7 @@ var con = mysql.createConnection({
   password: "mohit555",
   database: "mydbase",
 });
+
 // database connected confirmation
 con.connect(function (err) {
   if (err) throw err;
@@ -19,6 +21,7 @@ con.connect(function (err) {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //Add record using post
 app.post("/add", (req, res) => {
   let Name = req.body.name;
@@ -29,11 +32,12 @@ app.post("/add", (req, res) => {
     if (err) throw err;
     console.log("1 record inserted");
 
+    //increment 1 where operation add
     sql = "select count from counter where operation='add'";
     con.query(sql, (err3, rows) => {
       if (!err3) {
         temp = rows[0].count + 1;
-        sql = "update counter set count=" + temp + " where operation='add'";
+        sql = "update counter set count=" + temp + " where operation='add'";//update add by increment by 1
 
         con.query(sql, (err2, rows2) => {
           if (!err2) {
@@ -46,16 +50,13 @@ app.post("/add", (req, res) => {
   });
 });
 
-//update Table
+//update Table using put
 app.put("/update", (req, res) => {
   let Name = req.body.name;
   let Age = req.body.age;
-  var sql =
-    'update person set name="' +
-    Name +
-    '", age=' +
-    Age +
-    ' where name="aman" ';
+  let Id = req.body.id;
+  var sql ='update person set name="' +Name +'", age=' +Age +' where id="'+Id+'" ';
+
   console.log(sql);
   con.query(sql, function (err, result) {
     if (!err) {
@@ -63,7 +64,7 @@ app.put("/update", (req, res) => {
       con.query(sql, (err3, rows) => {
         if (!err3) {
           temp = rows[0].count + 1;
-          sql = "update counter set count=" + temp + " where operation='update'";
+          sql = "update counter set count=" + temp + " where operation='update'";//update update by increment by 1
 
           con.query(sql, (err2, rows2) => {
             if (!err2) {
@@ -75,9 +76,9 @@ app.put("/update", (req, res) => {
       });
     } else console.log(err);
   });
-
-  // con.query("select adder from count")
 });
+
+//count query using get
 app.get("/count", (req, res) => {
   var sql = "select * from counter";
   con.query(sql, (err, rows) => {
@@ -88,45 +89,9 @@ app.get("/count", (req, res) => {
     } else console.log(err);
   });
 });
-app.get("/updatecount", (req, res) => {
-    var sql = "select count from counter where operation='update'";
-    con.query(sql, (err, rows) => {
-      if (!err) {
-        temp = rows;
-        console.log(temp[0].count);
-        res.send(rows);
-      } else console.log(err);
-    });
-  });
-app.get("/pp", (req, res) => {
-  var sql2 = "update counter set count=5 where operation='add'";
-  con.query(sql2, (err2, rows2) => {
-    if (!err2) {
-      console.log("string");
-    } else console.log(err2);
-  });
-});
-
-app.get("/cc", (req, res) => {
-  var sql = "select count from counter where operation='add'";
-  con.query(sql, (err3, rows) => {
-    if (!err3) {
-      temp = rows[0].count + 1;
-      var sql2 = "update counter set count=" + temp + " where operation='add'";
-
-      con.query(sql2, (err2, rows2) => {
-        if (!err2) {
-          console.log("string");
-          res.send("chal");
-        } else console.log(err2);
-      });
-      // console.log(temp)
-    } else console.log(err3);
-  });
-});
 
 //fetching data
-app.get("/try", (res, req) => {
+app.get("/try", (req, res) => {
   con.query("select * from person", (err, rows, fields) => {
     if (!err) {
       console.log(rows);
