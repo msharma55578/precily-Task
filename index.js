7fdb6fc74beeaf97943633324687553e8f44c849
msharma55578 <m.sharma55578@gmail.com>
2020-11-03 19:26:01 +0530

    insert Task

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+let cors=require('cors');
+app.use(cors())
 let bodyParser=require('body-parser');
 app.use(bodyParser())
 // Connect create with Mysql
@@ -16,20 +18,21 @@ var con = mysql.createConnection({
     console.log("Connected!");
   });
   
+app.use(bodyParser.urlencoded({extended:true}))
 //Add record using post 
 app.post("/add",(req,res)=>{
-    let data=req.body
-    var sql="INSERT INTO try_table (name,age) VALUES ('Chander', '22')";
-    console.log(data)
+    let Name=req.body.name
+    let Age=req.body.age
+    var sql="INSERT INTO try_table (name,age) VALUES (?, ?)";
 
-    con.query(sql, function (err, result) {
+    con.query(sql,[Name,Age], function (err, result) {
     if (err) throw err;
       console.log("1 record inserted");
     });
   })
 
 //update Table
-app.put("/update",(res,req)=>{
+app.put("/update",(req,res)=>{
     con.query('update try_table set name="change", age="18" where name="chander" ',(err,rows,fields)=>{
         if(!err)
             console.log(rows);
